53c5d11946fadf1815a690195169e6264ceb9d24
Nikhil Gahlawat <nikhilgahlawat@Nikhils-MacBook-Pro.local>
2024-07-10 23:33:06 -0400

    create ./data if it doesn't exist

--- src/scotus_scraper/justia_case_urls.py
+++ src/scotus_scraper/justia_case_urls.py
@@ -4,7 +4,7 @@ import re
 from datetime import datetime
 import json
 from concurrent.futures import ProcessPoolExecutor
-from scotus_scraper.config import JUSTIA_YEAR_URL, START_YEAR, END_YEAR, CASELIST
+from scotus_scraper.config import JUSTIA_YEAR_URL, START_YEAR, END_YEAR, CASELIST, DATA_DIR
 
 def process_page(url):
   print('Parsing', url)
@@ -39,6 +39,7 @@ def main():
   end_year = END_YEAR
   years = list(range(start_year, end_year+1))
   output_file = CASELIST
+  data_dir = DATA_DIR
 
   urls = [url_base + str(y) + '.html' for y in years]
 
@@ -46,6 +47,10 @@ def main():
     caselist = list(executor.map(process_page, urls))
 
   caselist = [item for sublist in caselist for item in sublist]
+
+  if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+    print(f"Directory '{data_dir}' created.")
 
   with open (output_file, 'w') as json_file:
     json.dump(caselist, json_file, indent=2)
